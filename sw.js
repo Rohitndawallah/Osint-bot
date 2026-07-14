72263d1fed3eb459ed86a8bd1bef699a32531269
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-07-14 12:36:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = 'devcoderz-v1';
+const CACHE_NAME = 'devcoderz-v3';
 const ASSETS = [
   '/',
   '/index.html',
-  'https://cdn.tailwindcss.com'
+  '/batches.html',
+  '/chapters.html',
+  '/lectures.html',
+  '/Player/player.html',
+  '/Player/player2.html',
+  '/Player/player3.html'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (event) => {
@@ -24,9 +25,7 @@ self.addEventListener('fetch', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
+      return Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)));
     })
   );
 });
